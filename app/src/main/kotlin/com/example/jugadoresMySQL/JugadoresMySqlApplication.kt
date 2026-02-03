bdbed07b6bc223cfde925ba2420dee2fe47bc7fd
globo <diegoinsti2019@gmail.com>
2026-02-03 15:51:54 +0100

    Hecho cambios

---
+++ app/src/main/kotlin/com/example/jugadoresMySQL/JugadoresMySqlApplication.kt
@@ -0,0 +1,11 @@
+package com.example.jugadoresMySQL
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JugadoresMySqlApplication
+
+fun main(args: Array<String>) {
+	runApplication<JugadoresMySqlApplication>(*args)
+}
---
+++ app/src/test/kotlin/com/example/jugadoresMySQL/JugadoresMySqlApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.jugadoresMySQL
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class JugadoresMySqlApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
